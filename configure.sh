a6af027b7f934fb3a6cd9527df19488177c1e9fe
Carlos Dalla <46069684+carloseduardodalla@users.noreply.github.com>
2020-05-09 16:11:33 -0300

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,14 @@
 #!bin/bash
 
-## Configuration - Linux Ubuntu 18.04 ##
+## Configuration - Linux Ubuntu 18.04
+
+# Install INKSCAPE, TEXSTUDIO, TEXLIVE, OKULAR 
+sudo snap install inkscape -y &&
+sudo snap install texstudio -y &&
+sudo apt install texlive-full -y
+sudo snap install okular -y &&
+
+
+
 
 
