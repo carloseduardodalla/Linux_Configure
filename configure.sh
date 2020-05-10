6f2379d2ba2fe3ab08e9e6742e2f7e34dcbe667c
Carlos Dalla <46069684+carloseduardodalla@users.noreply.github.com>
2020-05-10 09:42:21 -0300

    Insert RISE and NBEXTENSIONS

--- configure.sh
+++ configure.sh
@@ -3,12 +3,17 @@
 ## Configuration - Linux Ubuntu 18.04
 
 # Install INKSCAPE, TEXSTUDIO, TEXLIVE, OKULAR 
-sudo snap install inkscape -y &&
-sudo snap install texstudio -y &&
-sudo apt install texlive-full -y
-sudo snap install okular -y &&
+  sudo snap install inkscape -y &&
+  sudo snap install texstudio -y &&
+  sudo apt install texlive-full -y
+  sudo snap install okular -y &&
 
+## ANACONDA Install
+  # Download at https://www.anaconda.com/ and run executable
+  conda activate #enviroment (base)
 
+  # RISE (for Jupyter-Notebook Presentations)
+  conda install -c conda-forge rise
 
-
-
+  # NBEXTENSIONS (add extensions in Jupyter-Notebook)
+  conda install -c conda-forge jupyter_contrib_nbextensions
