1b0ca1e7c9c45e87fe893aa70e7813b71c7fa4a6
Carlos Dalla <46069684+carloseduardodalla@users.noreply.github.com>
2020-05-12 23:40:21 -0300

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,11 +2,15 @@
 
 ## Configuration - Linux Ubuntu 18.04
 
-# Install INKSCAPE, TEXSTUDIO, TEXLIVE, OKULAR 
+# Install INKSCAPE, TEXSTUDIO, TEXLIVE, OKULAR
   sudo snap install inkscape -y &&
   sudo snap install texstudio -y &&
   sudo apt install texlive-full -y &&
   sudo snap install okular -y &&
+
+# Install VSCODE, DROPBOX
+  sudo snap install vscode -y &&
+  sudo snap install dropbox -y &&
 
 ## ANACONDA Install
   # Download at https://www.anaconda.com/ and run executable
